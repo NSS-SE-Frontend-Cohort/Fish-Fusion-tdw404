2bac2809801dffe868f4733aefbdfb47cb518064
Tom Walsh <walsth@gmail.com>
2024-11-08 09:11:40 -0600

    Updated map in restaurant js to produce array

--- restaurant.js
+++ restaurant.js
@@ -1,7 +1,7 @@
 const {mongerInventory} = require("./fishMonger.js")
 
 const fishMenu = (maxPrice) => {
-    var result = '<h1>Menu</h1>' + `\n` + `<article class="menu">`
+    var result = `<h1>Menu</h1>\n<article class="menu">\n`
     console.log("result")
 
     //The original for/of loop:
@@ -13,13 +13,9 @@ const fishMenu = (maxPrice) => {
     // }
 
     //Updated using the map() method
-    mongerInventory(maxPrice).map(fish => {
-        result = result + `\n` + `   <h2>${fish.species}</h2>`
-        result = result + `\n` + `   <section class="menu__item">${fish.species} Soup</section>`
-        result = result + `\n` + `   <section class="menu__item">${fish.species} Sandwich</section>`
-        result = result + `\n` + `   <section class="menu__item">Grilled ${fish.species}</section>`
-    })
-    result = result + `\n` + `</article>`
+    const menuItems = mongerInventory(maxPrice).map(fish => `   <h2>${fish.species}</h2>\n   <section class="menu__item">${fish.species} Soup</section>\n   <section class="menu__item">${fish.species} Sandwich</section>\n   <section class="menu__item">Grilled ${fish.species}</section>`)
+
+    result = result + menuItems.join(`\n`) + `\n</article>`
     return result;
 }
 
